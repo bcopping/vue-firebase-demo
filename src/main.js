6c00c2a1354f1b21b15647bd4952ec763f54e084
bcopping <ben@bjcdesign.com>
2017-07-05 15:26:56 +0100

    switch to full webpack build cli

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import App from './App'
 import VueRouter from 'vue-router'
 
 import VueFire from 'vuefire'
@@ -6,8 +7,6 @@ import GSignInButton from 'vue-google-signin-button'
 
 import { DatePicker, Table, TableColumn, Dialog, Button, Tooltip, Notification} from 'element-ui'
 
-
-import App from './App.vue'
 import { routes } from './routes'
 import { store } from './store/store';
 
@@ -39,6 +38,8 @@ const router = new VueRouter({
 	routes,
   mode: 'history',
 });
+
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
